f7383fcf14ff900d28a9c13665bb2baffcd49f21
Thiaguinhopbi <tdsrogerio@gmail.com>
2023-01-07 20:00:46 -0300

    Atualização de mensagem

---
+++ primeiro-projeto-java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Seja bem vindo! Estamos trabalhando para melhorar o conteúdo neste repositório!");
+    }
+}
